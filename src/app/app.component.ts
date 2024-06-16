f6d3f50e9bc06ab33bf485715218699d40fc50e9
Jack <nhattruong2513@gmail.com>
2024-06-16 23:27:27 +0700

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { TonConntectService } from './services/ton-connect.service';
+import TonWeb from 'tonweb';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'app-test';
+  address = '';
+  constructor(private tonConnectService: TonConntectService) {
+
+  }
+
+  ngOnInit(): void {
+    this.checkConnection()
+  }
+
+  checkConnection() {
+    this.tonConnectService.connected().then(r => {
+      if (r) {
+        this.address = (new TonWeb.Address(this.tonConnectService.currentTonWalletInfo?.account?.address)).toString(true, true, true, false);
+      }
+      else {
+        this.address = '';
+      }
+    })
+  }
+
+  async disconnect() {
+    await this.tonConnectService.disconnect();
+    this.checkConnection();
+  }
+
+  async connectWallet() {
+    const walletInfor: any = await this.tonConnectService.connect();
+    this.address = (new TonWeb.Address(walletInfor?.account?.address)).toString(true, true, true, false);
+  }
+}
---
+++ src/app/services/ton-connect.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from "@angular/core";
+import { TonConnectUI } from "@tonconnect/ui";
+
+@Injectable()
+export class TonConntectService {
+    private instance!: TonConnectUI;
+    public currentTonWalletInfo: any;
+    constructor() {
+        if (!this.instance) {
+            this.instance = new TonConnectUI({
+                manifestUrl: 'https://asset.battlecity.io/Metadata/tonconnect-mainfest.json',
+            });
+
+            this.instance.uiOptions = {
+                actionsConfiguration: {
+                    twaReturnUrl: 'https://t.me/DAPP_HANGARX_BOT/hangarx'
+                },
+                walletsListConfiguration: {
+                }
+            };
+        }
+    }
+
+    async connected() {
+        const connected = await this.instance.connectionRestored;
+        if (connected) {
+            this.currentTonWalletInfo = this.instance;
+        }
+        return connected;
+    }
+
+    async disconnect() {
+        await this.instance.disconnect();
+        this.currentTonWalletInfo = {};
+    }
+
+    async connect() {
+        const restored = await this.instance.connectionRestored;
+        if (restored) {
+            this.currentTonWalletInfo = this.instance;
+            return this.currentTonWalletInfo
+        }
+        await this.instance.openModal();
+
+        return new Promise(resolve => {
+            this.instance.onStatusChange(
+                walletAndwalletInfo => {
+                    // update state/reactive variables to show updates in the ui
+                    this.currentTonWalletInfo = walletAndwalletInfo;
+                    console.log(this.currentTonWalletInfo);
+
+                    resolve(this.currentTonWalletInfo)
+                }
+            );
+        })
+
+    }
+
+}
